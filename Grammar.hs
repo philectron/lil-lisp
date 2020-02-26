067eb10f3a4c5f11d314ff2bb9917a6b1a8efc1d
Sebastian <sebastiancbenjamin@gmail.com>
2020-02-25 18:01:57 -0800

    Finish grammar

--- Grammar.hs
+++ Grammar.hs
@@ -4,16 +4,19 @@ data ArithOp = Add | Sub | Mult | Div
 
 data BoolOp = Not | GT | LT | GTE | LTE
 
+data ListOp = ListConcat | Map Expr | Ind Int
+
 data Expr = If Expr Expr Expr               -- (if boolean-expr then-expr else-expr)
           | B Bool 
           | I Int 
           | N Name 
           | S String
-          | Concat Expr Expr                -- (++ string-expr string-expr)
+          | StrConcat Expr Expr             -- (++ string-expr string-expr)
           | ArithExpr ArithOp Expr Expr     -- ([+, -, *, /] num-expr num-expr)
           | BoolExpr BoolOp Expr Expr       -- ([<, >, <=, >=] bool-expr bool-expr)
           | Let [(Expr, Expr)] Expr         -- (let ((name-expr val-expr) (name-expr val-expr) (name-expr val-expr) ...) (body-expr))
-          | List [Expr]                     -- (expr1 expr2 expr3 ...)
+          | List [Expr]                     -- (list expr1 expr2 expr3 ...)
+          | ListExpr ListOp Expr            -- ([++, map <function>, ind <index value>] (list expr1 expr2 ...))
           | Call Stmt [Expr]                -- (function-name param1 param2 param3 ...)
 
 data Stmt = Func Expr [Expr] Expr           -- (fn name-expr (param-expr1 param-expr2 ...) (body-expr))
