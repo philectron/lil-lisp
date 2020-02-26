b1c7e20e66fd01f33dbc411dbfb61fe4451a821b
Phi Luu <philectron@users.noreply.github.com>
2020-02-26 13:54:41 -0800

    Reorganize grammars
    User deriving (Eq, Show) for everything

--- Grammar.hs
+++ Grammar.hs
@@ -3,29 +3,37 @@ module Grammar where
 type Name = String
 
 -- Aritmetic operations
-data ArithOp = Add | Sub | Mult | Div
+data ArithOp = Add | Sub | Mul | Div
+  deriving (Eq, Show)
 
 -- Boolean operations
-data BoolOp = Not | GT | LT | GTE | LTE
+data BoolOp = Not | EQ | GT | LT | GTE | LTE
+  deriving (Eq, Show)
 
--- List operations, categorized by whether they're unary or binary (if they take one list or two lists)
+-- List operations, categorized by whether they're unary or binary (if they take
+-- one list or two lists)
 data ListOpUn = Map Expr | Ind Int
+  deriving (Eq, Show)
 data ListOpBi = ListConcat
+  deriving (Eq, Show)
 
-data Expr = If Expr Expr Expr               -- (if boolean-expr then-expr else-expr)
-          | B Bool 
-          | I Int 
-          | N Name 
+data Expr = B Bool
+          | I Int
+          | N Name
           | S String
+          | If Expr Expr Expr               -- (if boolean-expr then-expr else-expr)
           | StrConcat Expr Expr             -- (++ string-expr string-expr)
           | ArithExpr ArithOp Expr Expr     -- ([+, -, *, /] num-expr num-expr)
-          | BoolExpr BoolOp Expr Expr       -- ([<, >, <=, >=] bool-expr bool-expr)
+          | BoolExpr BoolOp Expr Expr       -- ([!, =, >, <, >=, <=] bool-expr bool-expr)
           | Let [(Expr, Expr)] Expr         -- (let ((name-expr val-expr) (name-expr val-expr) (name-expr val-expr) ...) (body-expr))
           | List [Expr]                     -- (list expr1 expr2 expr3 ...)
           | ListExprUn ListOpUn Expr        -- ([map <function>, ind <index value>] (list expr1 expr2 ...))
           | ListExprBi ListOpBi Expr Expr   -- (++ (list expr1 expr2 ...) (list expr3 expr4 ...))
-          | Call Expr [Expr]                -- (function-name param1 param2 param3 ...)
+          | Call Expr [Expr]                -- (call function-name (param1 param2 param3 ...))
+  deriving (Eq, Show)
 
 data Stmt = Func Expr [Expr] Expr           -- (fn name-expr (param-expr1 param-expr2 ...) (body-expr))
+  deriving (Eq, Show)
 
 data Prog = Prog [Stmt] Expr                -- A list of function definitions and a call to a function called "main"
+  deriving (Eq, Show)
